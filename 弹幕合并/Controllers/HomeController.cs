3f402219de9658f83bdaeb9ff39d7104958bbc2f
dogvane <dogvane@gmail.com>
2018-11-12 21:27:37 +0800

    1.增加一个单独的，转换youtube字幕为b站字幕的页面

--- 弹幕合并/Controllers/HomeController.cs
+++ 弹幕合并/Controllers/HomeController.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using Dogvane.Srt;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using NETCore.Encrypt;
+using 弹幕合并.Bussiness.Entity;
 using 弹幕合并.Models;
 
 namespace 弹幕合并.Controllers
@@ -48,6 +52,11 @@ namespace 弹幕合并.Controllers
             return View();
         }
 
+        public IActionResult YoutubeToBilibili()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
@@ -75,6 +84,47 @@ namespace 弹幕合并.Controllers
             }
                 return Json("Hey");
             // return View("UpSrt");
+        }
+
+        [HttpPost]
+        public IActionResult YoutubeSrtFileToBilibliFile(IFormCollection files)
+        {
+            if (files.Files.Count == 0)
+                return Json("no file.");
+
+            var file = files.Files[0];
+            using (var stream = file.OpenReadStream())
+            {
+                var bytes = new byte[file.Length];
+                stream.Read(bytes, 0, (int) file.Length);
+
+                var lines = Encoding.UTF8.GetString(bytes).Split(@"\n");
+                var battutes = new SrtManagerT<TransBattuta>().LoadBattute(lines);
+
+                var bilibili = new BilibiliSrtFile();
+                bilibili.font_size = 0.4;
+                bilibili.font_color = "#FFFFFF";
+                bilibili.background_alpha = 0.5;
+                bilibili.background_color = "#9C27B0";
+                bilibili.Stroke = "none";
+                bilibili.body = new List<BilibiliSrtFile.Battuta>();
+
+                foreach (var item in battutes)
+                {
+                    bilibili.body.Add(new BilibiliSrtFile.Battuta
+                    {
+                        from = item.FromSec,
+                        to = item.ToSec,
+                        content = item.Text,
+                        location = 2,
+                    });
+                }
+
+                var json = JsonConvert.SerializeObject(bilibili);
+                bytes = Encoding.UTF8.GetBytes(json);
+
+                return File(bytes, "text/json", "(b站格式)" + file.FileName.Replace(".srt", ".bcc"));
+            }
         }
     }
 }
