685acf50729594c1bec93c66415dc4252553ded0
dogvane <dogvane@163.com>
2020-01-09 22:38:09 +0800

    增加从vtt到srt和b站字幕的文件转换工具

---
+++ VTTtoSrt/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using VTT;
+
+namespace VTTtoSrt
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+            if (args.Length == 0)
+            {
+                Console.WriteLine("请输入要转换的 srt 文件.");
+                return;
+            }
+
+            var fileName = args[0];
+
+            if (!File.Exists(fileName))
+            {
+                Console.WriteLine("文件不存在");
+                return;
+            }
+
+            var vtt = VTTHelper.GetVTTFromFile(fileName);
+
+            int id = 1;
+            StringBuilder writer = new StringBuilder();
+
+            foreach (var item in vtt.Lines)
+            {
+                var srtItem = new Dogvane.Srt.Battuta()
+                {
+                    Id = id++,
+                    From = item.From,
+                    Text =item.Text,
+                    To = item.To,
+                };
+                writer.AppendLine(srtItem.GetData());
+                writer.AppendLine();
+            }
+
+            var newFileName = fileName.Replace(".vvt", "") + ".srt";
+            File.WriteAllText(newFileName, writer.ToString(), Encoding.UTF8);
+        }
+    }
+}
